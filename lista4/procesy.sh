28d49bd6f328425be02e31baa11d112ef9ef18c6
mateusz <mateusznasewicz@gmail.com>
2022-11-23 19:29:57 +0100

    regex update

--- lista4/procesy.sh
+++ lista4/procesy.sh
@@ -4,8 +4,9 @@ result="PID PPID PGID SID COMM STATE TTY RSS FILE_COUNT\n"
 
 for process in /proc/[0-9]*
 do
-	stats=($(cat ${process}/stat))
-	result="${result}${stats[0]} ${stats[3]} ${stats[4]} ${stats[5]} ${stats[1]} ${stats[2]} ${stats[6]} ${stats[23]} $(sudo ls ${process}/fd/ | wc -l)\n"
+	stats=($(cat ${process}/stat | sed 's/([^*]*)//g' ))
+	comm=$(cat ${process}/stat | sed 's/.* (\(.*\)) .*/\1/g' | sed 's/ /-/g')
+	result="${result}${stats[0]} ${stats[2]} ${stats[3]} ${stats[4]} ${comm} ${stats[1]} ${stats[5]} ${stats[22]} $(sudo ls ${process}/fd/ | wc -l)\n"
 done
 result=$(printf "${result}" | column -t)
 printf "${result}\n"
