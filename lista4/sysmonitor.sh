5306efd66a6cc2115199923efba76e5bf75d5048
mateusz <mateusznasewicz@gmail.com>
2022-11-24 14:37:00 +0100

    ja pierdole

--- lista4/sysmonitor.sh
+++ lista4/sysmonitor.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
-net
+function avg {
+	sum=0
+	arr=("$@")
+	for i in "${arr[@]}"
+	do
+		sum=$(($sum+$i))
+	done
+	avg=$(echo "$sum/${#arr[@]}" | bc)
+	echo "$avg"
+}
+
+function chart_generator {
+
+}
+
+
+bytes_sent=()
+bytes_recieved=()
+
+download_speed_arr=()
+upload_speed_arr=()
+
+avg_download_speed_arr=()
+avg_upload_speed_arr=()
+
+counter=0
 
 while true
 do
+
+	bytes_sent+=($(cat /proc/net/dev | grep wlo1: | awk '{print $10}'))
+	bytes_recieved+=($(cat /proc/net/dev | grep wlo1: | awk '{print $2}'))
+	bytes_sent_avg+=($(avg "${bytes_sent[@]}"))
+	bytes_recieved_avg+=($(avg "${bytes_recieved[@]}"))
 
 	total_uptime=$(cat /proc/uptime | sed 's/\s.*$//')
 	uptime_day=$(echo "${total_uptime}/86400" | bc)
@@ -24,8 +54,27 @@ do
 		cpu_freq+=($(sudo cat /sys/devices/system/cpu/cpu$i/cpufreq/cpuinfo_cur_freq))
 	done
 
-	echo "${cpu_usage[*]}"
+	if [ ${#bytes_sent[@]} -gt 1 ] && [ ${#bytes_recieved[@]} -gt 1 ]
+	then
+		upload_speed=$(( ${bytes_sent[$counter]} - ${bytes_sent[$(($counter-1))]} ))
+		download_speed=$(( ${bytes_recieved[$counter]} - ${bytes_recieved[$(($counter-1))]} ))
 
+		download_speed_arr+=($download_speed)
+		upload_speed_arr+=($upload_speed)
+
+		avg_download_speed=$(avg "${download_speed_arr[@]}")
+		avg_upload_speed=$(avg "${upload_speed_arr[@]}")
+
+		avg_download_speed_arr+=($avg_download_speed)
+		avg_upload_speed_arr+=($avg_upload_speed)
+
+	fi
+
+	echo "download speed: ${download_speed}   ||  upload speed: ${upload_speed}"
+	echo "average download: ${avg_download_speed}  ||  average upload: ${avg_upload_speed}"
+
+	counter=$((${counter}+1))
 	sleep 1
 	clear
 done
+
